dbe305a6551c7fe34053c579b73f68bc9aac2f0a
vivek <vivekkushwaha46@gmail.com>
2020-04-11 17:08:37 -0600

    from video

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -8,16 +9,16 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div >
-          <div >
-            <img src={logo}  alt="logo" />
-            <h2>Welcome to React</h2>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Fib Calculator version 2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
-          </div>
+          </header>
           <div>
             <Route exact path="/" component={Fib} />
-            <Router path="/otherpage" component={OtherPage} />
+            <Route path="/otherpage" component={OtherPage} />
           </div>
         </div>
       </Router>
